3dbb1309ab14f68146fc0607b68b1bcda42d6aa7
Pablo Johnson <pablo.88j@gmail.com>
2016-09-28 14:53:41 -0500

    show welcome fragment

--- ShareBook/app/src/main/java/com/johnson/pablo/sharebook/ui/signIn/welcome/WelcomeActivity.java
+++ ShareBook/app/src/main/java/com/johnson/pablo/sharebook/ui/signIn/welcome/WelcomeActivity.java
@@ -3,18 +3,28 @@ package com.johnson.pablo.sharebook.ui.signIn.welcome;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
 
 import com.johnson.pablo.sharebook.R;
+import com.johnson.pablo.sharebook.ui.common.ShareBookActivity;
+import com.johnson.pablo.sharebook.ui.signIn.signIn.SignInFragment;
 
-public class WelcomeActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class WelcomeActivity extends ShareBookActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
 
+        if (savedInstanceState == null) {
+            replaceFragment(R.id.signUpContainer, WelcomeFragment.newInstance(), false);
+        }
 
     }
 
